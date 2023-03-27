f205d72b3fddc576acf89e09db7ecd18499f4d62
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2023-03-27 04:12:15 -0400

    Player Rank

--- SlothRace/Assets/Scripts/Environment/GameManager.cs
+++ SlothRace/Assets/Scripts/Environment/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 {
     // Singleton pattern
     public static GameManager S;
+    
+    [Header("Other Managers")]
     public GameObject playerInputManager;
     public DualSenseMonitor dualSenseMonitor;
     private PlayerInputManager _inputManager;
@@ -20,17 +22,21 @@ public class GameManager : MonoBehaviour
     }
     
     // public variables
-    public GameObject player1;
-    public GameObject player2;
-    public GameObject player3;
-    public GameObject player4;
+    [HideInInspector] public GameObject player1;
+    [HideInInspector] public GameObject player2;
+    [HideInInspector] public GameObject player3;
+    [HideInInspector] public GameObject player4;
 
     public bool player1Started;
     public bool player2Started;
+    public bool player3Started;
+    public bool player4Started;
 
     [Header("Spawn Positions")] 
     [SerializeField] private Vector3 player1SpawnPoint;
     [SerializeField] private Vector3 player2SpawnPoint;
+    [SerializeField] private Vector3 player3SpawnPoint;
+    [SerializeField] private Vector3 player4SpawnPoint;
 
     [Header("Player Join Status")]
     // maxPlayerCount: how many players can we have for the current level
@@ -39,6 +45,11 @@ public class GameManager : MonoBehaviour
     public int joinedPlayer;
     public int readyPlayer;
     public State gameState;
+
+    [Header("Player Rank")] 
+    public float[] playerDistances;
+    private int[] playerRank;
+    
     
     private void Awake()
     {
@@ -61,8 +72,14 @@ public class GameManager : MonoBehaviour
         EnableAndDisableJoin();
         ResetPlayer();
         CheckPlayerNum();
+        RankPlayers();
     }
 
+    private void RankPlayers()
+    {
+        return;
+    }
+    
     private void ResetPlayer()
     {
         if (player1 != null)
