d6862fccabc82e67b299dc0ec281bbd76d2f7486
Lingheng Tao <40527691+Lockbrains@users.noreply.github.com>
2023-03-27 20:32:10 -0400

    Player.cs Merge Fixed

---
+++ SlothRace/Assets/Scripts/Sloth/SpeedBoost.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // get player script
+            Player player = collision.gameObject.GetComponent<HipCamera>().player;
+
+            // add speed boost to player only if stack == 0
+            if (player.playerAbilities.Count == 0)
+            {
+                player.playerAbilities.Push("SpeedBoost");
+                Debug.Log("push speedboost to stack");
+            }
+            else
+            {
+                Debug.Log("you reached the limit of max number of abilities");
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
